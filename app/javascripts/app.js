cfc94df9568fd91b346089f7727ad671963f49e8
herecomesjaycee <jaycee.cheong04@gmail.com>
2017-07-23 11:07:41 +0100

    Claim is now loaded

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -2,14 +2,14 @@
 import "../stylesheets/app.css";
 
 // Import libraries we need.
-// import { default as Web3} from 'web3';
-// import { default as contract } from 'truffle-contract'
+import { default as Web3} from 'web3';
+import { default as contract } from 'truffle-contract'
 
 // Import our contract artifacts and turn them into usable abstractions.
 import claims_artifacts from '../../build/contracts/Claims.json'
 
 // MetaCoin is our usable abstraction, which we'll use through the code below.
-// var MetaCoin = contract(metacoin_artifacts);
+var Claim = contract(claims_artifacts);
 // The following code is simple to show off interacting with your contracts.
 // As your needs grow you will likely need to change its form and structure.
 // For application bootstrapping, check out window.addEventListener below.
@@ -18,14 +18,13 @@ var account;
 var address;
 var account_address;
 var company_account_address;
-var Claim;
 
 window.App = {
   start: function() {
     var self = this;
 
     // Bootstrap the Claim abstraction for Use.
-    //Claim.setProvider(web3.currentProvider);
+    Claim.setProvider(web3.currentProvider);
 
     // Get the initial account balance so it can be displayed.
     web3.eth.getAccounts(function(err, accs) {
@@ -39,17 +38,18 @@ window.App = {
         return;
       }
 
-      $.getJSON('../../build/contracts/Claims.json', function(data) {
-              console.log("loading claims json");
-              // Get the necessary contract artifact file and instantiate it with truffle-contract.
-              var ClaimArtifact = data;
-              App.contracts.Claim = TruffleContract(AdoptionArtifact);
+      console.log(Claim)
+      // $.getJSON('../../build/contracts/Claims.json', function(data) {
+      //         console.log("loading claims json");
+      //         // Get the necessary contract artifact file and instantiate it with truffle-contract.
+      //         var ClaimArtifact = data;
+      //         console.log(App.contracts);
+      //         App.contracts.Claim = TruffleContract(ClaimArtifact);
 
-              // Set the provider for our contract.
-              App.contracts.Claim.setProvider(App.web3Provider);
-              Claim = App.contracts.Claim;
-              console.log(App.contracts)
-            });
+      //         // Set the provider for our contract.
+      //         App.contracts.Claim.setProvider(App.web3Provider);
+      //         Claim = App.contracts.Claim;
+      //       });
 
       accounts = accs;
       account = accounts[0];
@@ -122,7 +122,8 @@ window.addEventListener('load', function() {
     console.warn("Using web3 detected from external source. If you find that your accounts don't appear or you have 0 MetaCoin, ensure you've configured that source properly. If using MetaMask, see the following link. Feel free to delete this warning. :) http://truffleframework.com/tutorials/truffle-and-metamask")
     // Use Mist/MetaMask's provider
     window.web3 = new Web3(web3.currentProvider);
-  } else {
+  } 
+  else {
     console.warn("No web3 detected. Falling back to http://localhost:8545. You should remove this fallback when you deploy live, as it's inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask");
     // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
     window.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
